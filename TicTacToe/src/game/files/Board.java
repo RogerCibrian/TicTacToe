e00eb711337dcd5bf1608e738adda0b66a3e5b2b
RogerCibrian <rcibrian@ubuntu>
2021-10-23 18:48:10 -0700

    Added initial attributes to Board class, created test methods.

--- TicTacToe/src/game/files/Board.java
+++ TicTacToe/src/game/files/Board.java
@@ -1,5 +1,26 @@
 package game.files;
 
 public class Board {
+	
+	private int columns;
+	private int rows;
+	private int[] state;
+	
+	
+	Board(int columns, int rows){
+		this.columns=columns;
+		this.rows=rows;
+	}
+	
+	void setInitialState()
+	{
+		state = new int[columns * rows];
+		for(int i=0; i < state.length; i++)
+		{
+			state[i]=0;
+			System.out.println(state[i]);
+		}
+	}
+	
 
 }
--- TicTacToe/src/game/files/TicTacToe.java
+++ TicTacToe/src/game/files/TicTacToe.java
@@ -3,7 +3,10 @@ package game.files;
 public class TicTacToe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Board ticTacToeBoard = new Board(3,3);
+		
+		ticTacToeBoard.setInitialState();
 
 	}
 
